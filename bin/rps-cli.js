d1a2ac8c4bc4b5b5cc55e12fb1a396ae1341ab44
wdtaylor30 <wdtaylor30@gmail.com>
2023-03-26 17:05:44 -0400

    added help and rules argument handling

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,30 +1,38 @@
 #!/usr/bin/env node
 
 // import minimist
-const args = require('minimist')(process.argv.slice(2));
+const args = require('minimist')(process.argv.slice(2), {
+    // define custom -r flag
+    alias: {
+        r: "rules"
+    }
+});
 
 // import written functions from /lib/rpsls.js
 const { playRPS, helpRPS, rulesRPS, playRPSLS, helpRPSLS, rulesRPSLS } = require("../lib/rpsls");
 
-// pull argument from command line and make it lowercase
-var playerMove = args._[0];
+// first check for help and rules flags
+if (args.h || args.help) {
+    helpRPS();
+    return;
+} else if (args.r || args.rules) {
+    rulesRPS();
+    return;
+} else {
+    // otherwise pull argument from command line
+    var playerMove = args._[0];
 
-// play rock if no argument is passed
-if (!playerMove) {
-    result = { "player": "rock"};
+    // play rock if no argument is passed
+    if (!playerMove) {
+        result = { "player": "rock"};
+        console.log(JSON.stringify(result));
+        return result;
+    }
+
+    // if an argument is passed, make it lowercase
+    playerMove = playerMove.toLowerCase();
+
+    // play the game by randomizing cpu and return result
+    result = playRPS(playerMove);
     console.log(JSON.stringify(result));
-    return result;
 }
-
-// if an argument is passed, convert it to lowercase
-playerMove = playerMove.toLowerCase();
-
-// TODO: Here, put logic for the -h and -r flags.
-
-// TODO: Initial thought is to put these in an if statement
-// TODO: before the playRPS call. The playRPS call will be an
-// TODO: else condition if the other flags aren't given.
-
-// then randomize the game and return the json result
-result = playRPS(playerMove);
-console.log(JSON.stringify(result));
