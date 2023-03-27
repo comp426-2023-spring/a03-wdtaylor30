c6e94c350a83e517d05ef3dcce5f222a74698c71
wdtaylor30 <wdtaylor30@gmail.com>
2023-03-26 21:29:35 -0400

    updated Node.js version to allow for imports

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,44 +1,48 @@
 #!/usr/bin/env node
 
 // import minimist
-const args = require('minimist')(process.argv.slice(2), {
-    // define custom -r flag
+import minimist from "minimist";
+
+// define custom -r flag
+const args = minimist(process.argv.slice(2), {
     alias: {
         r: "rules"
     }
 });
 
+
 // import written functions from /lib/rpsls.js
-const { playRPS, helpRPS, rulesRPS } = require("../lib/rpsls");
+// const { playRPS, helpRPS, rulesRPS } = require("../lib/rpsls");
+import { playRPS, helpRPS, rulesRPS } from "../lib/rpsls.js";
 
 // first check for help and rules flags
 if (args.h || args.help) {
     helpRPS();
-    return;
+    process.exit();
 } else if (args.r || args.rules) {
     rulesRPS();
-    return;
+    process.exit();
 } else {
     // otherwise pull argument from command line
     var playerMove = args._[0];
 
     // play rock if no argument is passed
     if (!playerMove) {
-        result = { "player": "rock" };
+        var result = { "player": "rock" };
         console.log(JSON.stringify(result));
-        return result;
+        process.exit();
     }
 
     // if an argument is passed, make it lowercase
     playerMove = playerMove.toLowerCase();
 
     // play the game by randomizing cpu and return result
-    result = playRPS(playerMove);
+    var result = playRPS(playerMove);
     
     // if defined, log to console, otherwise exit silently
     if (!(typeof result == "undefined")) {
         console.log(JSON.stringify(result));
-        return result;
+        process.exit();
     }
-    return;
+    process.exit();
 }
--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,43 +1,45 @@
 #!/usr/bin/env node
 
 // import minimist
-const args = require('minimist')(process.argv.slice(2), {
-    // define custom -r flag
+import minimist from "minimist";
+
+// define custom -r flag
+const args = minimist(process.argv.slice(2), {
     alias: {
         r: "rules"
     }
 });
 
 // import written functions from /lib/rpsls.js
-const { playRPSLS, helpRPSLS, rulesRPSLS } = require("../lib/rpsls");
+import { playRPSLS, helpRPSLS, rulesRPSLS } from "../lib/rpsls.js";
 
 // first check for help and rules flags
 if (args.h || args.help) {
     helpRPSLS();
-    return;
+    process.exit();
 } else if (args.r || args.rules) {
     rulesRPSLS();
-    return;
+    process.exit();
 } else {
     // otherwise pull argument from command line
     var playerMove = args._[0];
 
     // play rock if no argument is passed
     if (!playerMove) {
-        result = { "player": "rock" };
+        var result = { "player": "rock" };
         console.log(JSON.stringify(result));
-        return result;
+        process.exit();
     }
 
     // if an argument is passed, make it lowercase
     playerMove = playerMove.toLowerCase();
 
-    // play the game by randomizing cpu and return result
-    result = playRPSLS(playerMove);
+    // play the game by randomizing cpu and process.exit() result
+    var result = playRPSLS(playerMove);
     
     // if defined, log to console, otherwise exit silently
     if (!(typeof result == "undefined")) {
         console.log(JSON.stringify(result));
     }
-    return;
+    process.exit();
 }
